6bb27ae51a81098cc625f80053e4c8f0c6cc7d59
Alexander Regueiro <alexreg@gmail.com>
2021-02-16 00:01:51 +0000

    Check whether base dir is user-writable instead of created subdirs

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 FILE_FOLDER=`dirname $0`;
 FILE_NAME=`basename $0`;
-SUDO=""
+SUDO="";
 
 message() {
 	TYPE="$1";
@@ -19,45 +19,51 @@ message() {
 # Choose an installation folder
 if [ "$#" -gt "1" ]; then
 	message "ERROR" "Unhandled number of arguments passed to script.";
-	exit;
+	exit 1;
 elif [ "$#" -eq "1" ]; then
 	BASE_FOLDER="$1";
 	echo "using custom folder: ${BASE_FOLDER}";
 else
-	TEX_FOLDER="/usr/share/texmf-texlive"
-	BASE_FOLDER="/usr/local/share/texmf"
+	TEX_FOLDER="/usr/share/texmf-texlive";
+	BASE_FOLDER="/usr/local/share/texmf";
 	if [ ! -d "${TEX_FOLDER}" ]; then
-		TEX_FOLDER="/Library/TeX"
-		BASE_FOLDER="${HOME}/Library/texmf"
+		TEX_FOLDER="/Library/TeX";
+		BASE_FOLDER="${HOME}/Library/texmf";
 	fi
 	if [ ! -d "${TEX_FOLDER}" ]; then
 		TEX_FOLDER="/usr/local/texlive";
 		BASE_FOLDER="${TEX_FOLDER}";
 	fi
 	if [ ! -d "${TEX_FOLDER}" ]; then
-		message "ERROR" "Could not find a LaTeX installation folder, you're likely running an unhandled LaTeX distribution."
-		exit
+		message "ERROR" "Could not find a LaTeX installation folder, you're likely running an unhandled LaTeX distribution.";
+		exit 1;
 	fi
+fi
+
+# Check that base folder exists
+if [ ! -w "$BASE_FOLDER" ]; then
+	message "ERROR" "Directory `${BASE_FOLDER}` does not exist.";
+	exit 1;
 fi
 
 # Detect the package updater
 HASH_UPDATER=`which texhash`;
 if [ "${HASH_UPDATER}" = "" ]; then
 	message "ERROR" "The system is using an unknown TeX hash updating application.";
-	exit;
+	exit 1;
 fi
 
 INSTALL_LTX_FOLDER="${BASE_FOLDER}/tex/latex/jabbrv";
 INSTALL_BST_FOLDER="${BASE_FOLDER}/bibtex/bst/jabbrv";
 
 # Make sure the user has the appropriate permissions
-if [ ! -w $INSTALL_LTX_FOLDER ] || [ ! -w $INSTALL_BST_FOLDER ]; then
-	SUDO="sudo"
+if [ ! -w "$BASE_FOLDER" ]; then
+	SUDO="sudo";
 	SUDO_TEST=`$SUDO -n echo "test" 2> /dev/null`;
 	if [ "${SUDO_TEST}" != "test" ]; then
-		message "ERROR" "Super user permissions are required to continue, instead run:";
+		message "ERROR" "Super user permissions are required to continue; instead run:";
 		message "EMERR" "$SUDO ./${FILE_NAME}";
-		exit;
+		exit 1;
 	fi
 fi
 
@@ -71,4 +77,3 @@ $SUDO cp "${FOLDER}/"*.bst "${INSTALL_BST_FOLDER}/";
 
 # Update the package list
 sudo ${HASH_UPDATER};
-
